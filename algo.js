34d80ad8db45ba87a3a3697c4e3213354e502a81
steveneross94 <ross.steven@me.com>
2020-09-15 11:52:03 -0400

    first exercise

--- algo.js
+++ algo.js
@@ -1,1 +1,13 @@
 // worksheet for DS&A with Udemy
+
+const nemo = ['nemo']
+
+function findNemo(array){
+    for (let i = 0; i < array.length; i++){
+        if (array[i] === 'nemo'){
+            console.log("Found Nemo!");
+        }
+    }
+}
+
+findNemo(nemo);
