cde12cff16c4676f5d83ec1a770d352360134b63
steveneross94 <ross.steven@me.com>
2020-09-21 14:46:11 -0400

    ex3

--- algoEx3.js
+++ algoEx3.js
@@ -1,1 +1,15 @@
 // test 
+
+const boxes = [1,2,3,4,5];
+
+function logAllPairsOfArray(array){
+    for (let i = 0; i < array.length; i++){
+        for (let j = 0; j < array.length; j++){
+            console.log(array[i], array[j])
+        }
+    }
+}
+
+logAllPairsOfArray(boxes)
+
+// O(n^2)
