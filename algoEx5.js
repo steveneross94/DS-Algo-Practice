06be1ed98e7622ae7b7a1cd791372838576d506b
steveneross94 <ross.steven@me.com>
2020-09-25 14:42:17 -0400

    js specific solution

--- algoEx5.js
+++ algoEx5.js
@@ -4,7 +4,7 @@
 // should return true 
 
 const array1 = ['a', 'b', 'c', 'x']
-const array2 = ['z', 'y', 'x']
+const array2 = ['z', 'y', 'i']
 // false 
 
 // const checkIfCommonItemsInArray = (array1, array2) => {
@@ -29,23 +29,32 @@ const array2 = ['z', 'y', 'x']
 // array2 - check each item to the object and see if it exists
 // array2[array1[i]] === obj.properties
 
-const containsCommonItem = (arr1,arr2) => { // faster because non-nested loops
-    // loop through first array, and create object where properties === items in the array
-    // loop through second array and check if item in second array exists on created object
-    let obj = {}
-    for (let i = 0; i < arr1.length; i++){
-        if (!obj[i]){
-            let item = arr1[i]
-            obj[item] = true
-        }
-    }
+
+// there's a better way to accomplish the below with javascript 
+// const containsCommonItem = (arr1,arr2) => { // faster because non-nested loops
+//     // loop through first array, and create object where properties === items in the array
+//     // loop through second array and check if item in second array exists on created object
+//     let obj = {}
+//     for (let i = 0; i < arr1.length; i++){
+//         if (!obj[i]){
+//             let item = arr1[i]
+//             obj[item] = true
+//         }
+//     }
     
-    for (let j = 0; j < arr2.length; j++){
-        if (obj[arr2[j]]){
-            return true
-        }
-    }
-    return false
+//     for (let j = 0; j < arr2.length; j++){
+//         if (obj[arr2[j]]){
+//             return true
+//         }
+//     }
+//     return false
+// }
+
+// console.log(containsCommonItem(array1, array2))
+
+// JS specific solution
+function containsCommonItem3(arr1,arr2){
+    return arr1.some(item => arr2.includes(item))
 }
 
-console.log(containsCommonItem(array1, array2))
+console.log(containsCommonItem3(array1, array2))
