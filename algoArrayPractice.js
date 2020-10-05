2aef02426d05fe9af4abde9401cc1ea27fb9091e
steveneross94 <ross.steven@me.com>
2020-10-05 15:27:24 -0400

    simple array work - mostly already known

---
+++ algoArrayPractice.js
@@ -0,0 +1,15 @@
+const strings = ['a', 'b', 'c', 'd']
+                //0    1    2    3 
+//push 
+strings.push('e') //0(1)
+
+//pop
+strings.pop(); //0(1)
+
+//unshift
+strings.unshift('x') //O(n)
+
+//splice
+strings.splice(2, 0, 'alien') //O(n/2) = adjusts to => O(n)
+
+console.log(strings)
